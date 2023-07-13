ee81128ba51746b7d7c4079e997f32cc30f675ac
Anas Khan <84630559+Makorg123@users.noreply.github.com>
2023-07-13 15:51:20 +0530

    Update prediction.py

--- prediction.py
+++ prediction.py
@@ -1,6 +1,6 @@
 import joblib
 
 def predict(data):
-      model = joblib.load('rf_model.sav')
+      model = joblib.load('reg_model.sav')
       prediction = model.predict(data)
       return prediction
